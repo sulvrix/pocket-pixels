c14fb25de21f04674d3e0075fdea8c1386934c21
Sultan Salah <sultanbamarhool@gmail.com>
2025-05-27 17:20:43 +0300

    Deployment2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import PocketDex from "./PocketDex";
 import PokemonDetails from './components/PokemonDetails';
 
