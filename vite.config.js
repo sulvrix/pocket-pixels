73f421c4f5058c23f654def664637f73c342cfb8
Sultan Salah <sultanbamarhool@gmail.com>
2025-05-30 01:13:32 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,17 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import ghPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),
+  tailwindcss(),
+  ghPages({
+    branch: 'gh-pages',
+    dotfiles: true
+  })
+  ],
+  base: 'pocket-pixels',
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
